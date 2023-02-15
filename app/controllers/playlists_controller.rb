9cf85c33887b3b8424f7356ac285a70d007d6102
garammasala29 <kmasa1569@gmail.com>
2023-02-15 16:10:20 +0900

    Playlistに保存されているものからSpotify Playlistを作成した

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -3,8 +3,14 @@ class PlaylistsController < ApplicationController
   end
 
   def spotify
-    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    playlist = RSpotify::Playlist.find_by_id(ENV['PLAYLIST_ID'])
-    chart_playlist = Playlist.scrape
+    RSpotify::User.new(request.env['omniauth.auth'])
+    spotify_playlist = RSpotify::Playlist.find_by_id(ENV['PLAYLIST_ID'])
+    positions = (0..spotify_playlist.total - 1).to_a
+    spotify_playlist.remove_tracks!(positions, snapshot_id: spotify_playlist.snapshot_id)
+    # Playlist.scrape
+    tracks = Playlist.all.flat_map do |list|
+      RSpotify::Track.search("artist:#{list.artist} track:#{list.track}", limit: 1).flatten
+    end.compact_blank
+    spotify_playlist.add_tracks!(tracks)
   end
 end
